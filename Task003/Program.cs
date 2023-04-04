592c054f2f648a2666387e6329c6b0c7c7252f26
MedOK99 <127406946+MedOK99@users.noreply.github.com>
2023-04-04 21:38:28 +0300

    Добавил решение задачи 29. Как решил - сам не понял)

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,13 +1,47 @@
 ﻿/*  Задача 29: Напишите программу, которая приниматет 
-на вход длину массива, минимальное значение массива и максимальное значение массива, создаёт массив с заданными параметрами и выводит значения на экран
+на вход длину массива, минимальное значение массива и 
+максимальное значение массива, создаёт массив с заданными 
+параметрами и выводит значения на экран
 
 5, 1, 19 -> [1, 2, 5, 7, 19]
 
+Т.е:
+Введите длину массива: 4
+Введите минимальное допустимое значение элемента массива: 1
+Введите максимальное допустимое значение элемента массива: 33
+
 3, 1, 33 -> [6, 1, 33]
 */
+Console.Clear();
+Console.WriteLine("Введите длину массива данных:");
+int lenght = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите минимальное допустимое значение массива:");
+int min = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите максимальное допустимое значение массива:");
+int max = int.Parse(Console.ReadLine());
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    for (int i = 0; i < length; i++)
+    {
+        collection[i] = new Random().Next(min, max);
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count - 1)
+    {
+        Console.Write($"{col[position]}, ");
+        position++;
+    }
+    Console.Write($"{col[position]}");
+}
 
-
-
+int[] array = new int[lenght];
+FillArray(array);
+PrintArray(array);
 
 
 
